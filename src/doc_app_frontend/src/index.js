df646097529a87f37094b7860d4f939855064642
mark <111102024+markmatakili2@users.noreply.github.com>
2024-02-15 18:00:28 +0300

    Health app

---
+++ src/doc_app_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { doc_app_backend } from "../../declarations/doc_app_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await doc_app_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
